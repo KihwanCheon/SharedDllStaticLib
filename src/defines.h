eab943690b4a5c5422b5f8d45aa146a787a3c1c1
KihwanCheon <gujjy97@gmail.com>
2019-06-02 19:30:13 +0900

    Modify msvc dll preprocessor macro, Show External Dependencies list

--- src/defines.h
+++ src/defines.h
@@ -1,15 +1,10 @@
 
 
-#ifdef WIN32
-#ifndef STATIC_LIB
+#if defined(WIN32) && !defined(STATIC_LIB)
 #ifdef SHARED_DLL_EXPORTS
 #define DLL_API __declspec(dllexport)
 #else
 #define DLL_API __declspec(dllimport)
-#endif
-
-#else
-#define DLL_API
 #endif
 #else
 #define DLL_API
